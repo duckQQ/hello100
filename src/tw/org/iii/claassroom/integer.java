fa9a621c4b2d11588496ad34d720540de1902c69
duckQQ <ukpiyo@gmail.com>
2016-08-16 19:14:28 +0800

    tell which year has 29 days in Feb

---
+++ src/tw/org/iii/claassroom/integer.java
@@ -0,0 +1,11 @@
+package tw.org.iii.claassroom;
+
+public class integer {
+
+	public static void main(String[] args) {
+		byte a = 10, b = 3;
+		int c = a / b;
+		System.out.println(c);
+	}
+
+}
---
+++ src/tw/org/iii/claassroom/year.java
@@ -0,0 +1,31 @@
+package tw.org.iii.claassroom;
+
+import java.util.Scanner;
+
+public class year {
+
+	public static void main(String[] args) {
+		//int Year = (int)(Math.random()*3000+1); //1-3000
+		//System.out.println(Year);
+		System.out.println("請輸入年份：");
+		int Year;
+		Scanner scanner = new Scanner(System.in);
+		Year = scanner.nextInt();
+		if (Year%4 != 0){
+			System.out.println("平年4");
+		} else {
+			if(Year%100 != 0){
+				System.out.println("閏年100");
+			}else {
+				if(Year%400 !=0){
+					System.out.println("平年400");
+				}else{
+					System.out.println("閏年400");
+				}
+				
+			}
+		}
+
+	}
+
+}
